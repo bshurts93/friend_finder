a43c314de3871701fbc5b4a1a9daf9b409bb12d2
Ben Shurts <bshurts93@gmail.com>
2019-04-18 18:33:49 -0500

    Initial seed data into friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,37 @@
+[
+    {
+        name: "Steven",
+        photo: "photoURL",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        name: "Julie",
+        photo: "photoURL",
+        scores: [
+            3,
+            2,
+            3,
+            3,
+            1,
+            2,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+
+]
+
+
